1054de6aeafbabd18b4ed83b6841a782622870e7
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-06-22 18:26:40 +0300

    Initial commit

---
+++ src/a/b/c/Boo1.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 6/22/2018.
+ * Project: aaa
+ * *******************************
+ */
+public class Boo1 {
+}
